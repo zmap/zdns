4d12e5cf5a7eb4c93ce669e75d55372d0200ecd4
Zakir Durumeric <zakird@gmail.com>
2016-07-05 12:25:55 -0600

    undeleting lookup

---
+++ interface.go
@@ -0,0 +1,79 @@
+package zdns
+
+import (
+	"flag"
+	_"log"
+	"strings"
+)
+
+type Lookup interface {
+
+	DoLookup(name string) (interface {}, error)
+
+}
+
+type GenericLookup struct {
+
+	NameServers *[]string
+	Timeout int
+}
+
+
+type LookupFactory interface {
+	// expected to add any necessary commandline flags if being
+	// run as a standalone scanner
+	AddFlags(flags *flag.FlagSet) error
+	// global initialization. Gets called once globally
+	// This is called after command line flags have been parsed
+	Initialize(conf *GlobalConf) error
+	// We can't set variables on an interface, so write functions
+	// that define any settings for the factory
+	AllowStdIn() (bool)
+	// Return a single scanner which will scan a single host
+	MakeLookup() (Lookup, error)
+
+}
+
+type GenericLookupFactory struct {
+
+	NameServers *[]string
+	Timeout int
+}
+
+func (l GenericLookupFactory) Initialize(c *GlobalConf) error {
+	return nil
+}
+
+func (s GenericLookupFactory) AllowStdIn() bool {
+	return true
+}
+
+
+// keep a mapping from name to factory
+var lookups map[string]LookupFactory;
+
+func RegisterLookup(name string, s LookupFactory) {
+	if lookups == nil {
+		lookups = make(map[string]LookupFactory, 100)
+	}
+	lookups[name] = s
+}
+
+func ValidlookupsString() string {
+
+	valid := make([]string, len(lookups))
+	for k, _ := range lookups {
+		valid = append(valid, k)
+		//log.Debug("loaded module:", k)
+	}
+	return strings.Join(valid, ", ")
+}
+
+func GetLookup(name string) LookupFactory {
+
+	factory, ok := lookups[name]
+	if !ok {
+		return nil
+	}
+	return factory
+}
--- lookup.go
+++ lookup.go
@@ -1,79 +1,77 @@
 package zdns
 
 import (
-	"flag"
-	_"log"
+	"os"
+_	"io"
 	"strings"
+	"bufio"
+	"log"
 )
 
-type Lookup interface {
+func makeName(name string, prefix string) string {
 
-	DoLookup(name string) (interface {}, error)
-
-}
-
-type GenericLookup struct {
-
-	NameServers *[]string
-	Timeout int
-}
-
-
-type LookupFactory interface {
-	// expected to add any necessary commandline flags if being
-	// run as a standalone scanner
-	AddFlags(flags *flag.FlagSet) error
-	// global initialization. Gets called once globally
-	// This is called after command line flags have been parsed
-	Initialize(conf *GlobalConf) error
-	// We can't set variables on an interface, so write functions
-	// that define any settings for the factory
-	AllowStdIn() (bool)
-	// Return a single scanner which will scan a single host
-	MakeLookup() (Lookup, error)
+	if prefix == "" {
+		return name
+	} else {
+		return strings.Join([]string{prefix, name}, "")
+	}
 
 }
 
-type GenericLookupFactory struct {
+func doLookup(f *LookupFactory, gc *GlobalConf, input <-chan string, output chan<- string) error {
 
-	NameServers *[]string
-	Timeout int
-}
+	for rawName := range input {
+		lookupName := makeName(rawName, gc.NamePrefix)
+		l, err := f.MakeLookup()
+		if err {
 
-func (l GenericLookupFactory) Initialize(c *GlobalConf) error {
+		}
+		res, err := f.DoLookup(lookupName)
+	}
 	return nil
 }
 
-func (s GenericLookupFactory) AllowStdIn() bool {
-	return true
+// write results from lookup to output file
+func doOutput(out <-chan string, path string) error {
+	var f *os.File;
+	if path == "" || path == "-" {
+		f = os.Stdout
+	} else {
+		f, err := os.Open(path)
+	}
+	for n := range out {
+		f.WriteString(n)
+		f.WriteString("\n")
+	}
+	return nil
+}
+
+// read input file and put results into channel
+func doInput(in chan<- string, path string) error {
+	var f *os.File;
+	if path == "" || path == "-" {
+		f = os.Stdin
+	} else {
+		f, err := os.Open(path)
+	}
+	s := bufio.NewScanner(f)
+    for s.Scan() {
+		in <- s.Text()
+    }
+    if err := s.Err(); err != nil {
+        log.Fatal("input unable to read file", err)
+    }
+	return nil
 }
 
 
-// keep a mapping from name to factory
-var lookups map[string]LookupFactory;
 
-func RegisterLookup(name string, s LookupFactory) {
-	if lookups == nil {
-		lookups = make(map[string]LookupFactory, 100)
-	}
-	lookups[name] = s
-}
+func DoLookups(f *LookupFactory, c *GlobalConf) error {
 
-func ValidlookupsString() string {
+	inChan := make(chan string)
+	outChan := make(chan string)
 
-	valid := make([]string, len(lookups))
-	for k, _ := range lookups {
-		valid = append(valid, k)
-		//log.Debug("loaded module:", k)
-	}
-	return strings.Join(valid, ", ")
-}
+	go doOutput(outChan, c.OutputFilePath)
+	return nil
 
-func GetLookup(name string) LookupFactory {
-
-	factory, ok := lookups[name]
-	if !ok {
-		return nil
-	}
-	return factory
 }
