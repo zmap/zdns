d4d091f410e580fae562d540e464fcc5f5962d58
Zakir Durumeric <zakird@gmail.com>
2016-07-13 16:58:43 -0700

    missing an include

---
+++ modules/cname/cname.go
@@ -0,0 +1,78 @@
+/*
+ * ZDNS Copyright 2016 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package a
+
+import (
+	"github.com/miekg/dns"
+	"github.com/zmap/zdns"
+	"github.com/zmap/zdns/modules/miekg"
+)
+
+// Per Connection Lookup ======================================================
+//
+type Lookup struct {
+	Factory *RoutineLookupFactory
+	miekg.Lookup
+}
+
+func ParseA(res dns.RR) (miekg.Answer, bool) {
+	if a, ok := res.(*dns.CNAME); ok {
+		return miekg.Answer{a.Hdr.Ttl, dns.Type(a.Hdr.Rrtype).String(), a.Target}, true
+	}
+	return miekg.Answer{}, false
+}
+
+func (s *Lookup) DoLookup(name string) (interface{}, zdns.Status, error) {
+	nameServer := s.Factory.Factory.RandomNameServer()
+	return miekg.DoLookup(s.Factory.Client, s.Factory.TCPClient, nameServer, ParseA, dns.TypeCNAME, name)
+}
+
+// Per GoRoutine Factory ======================================================
+//
+type RoutineLookupFactory struct {
+	miekg.RoutineLookupFactory
+	Factory *GlobalLookupFactory
+}
+
+func (s *RoutineLookupFactory) MakeLookup() (zdns.Lookup, error) {
+	a := Lookup{Factory: s}
+	return &a, nil
+}
+
+// Global Factory =============================================================
+//
+type GlobalLookupFactory struct {
+	zdns.BaseGlobalLookupFactory
+}
+
+// Command-line Help Documentation. This is the descriptive text what is
+// returned when you run zdns module --help
+func (s *GlobalLookupFactory) Help() string {
+	return ""
+}
+
+func (s *GlobalLookupFactory) MakeRoutineFactory() (zdns.RoutineLookupFactory, error) {
+	r := new(RoutineLookupFactory)
+	r.Factory = s
+	r.Initialize(s.GlobalConf.Timeout)
+	return r, nil
+}
+
+// Global Registration ========================================================
+//
+func init() {
+	s := new(GlobalLookupFactory)
+	zdns.RegisterLookup("CNAME", s)
+}
