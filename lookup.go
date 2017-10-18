5ca2c971946484e4d7569e46f118fe8dcafc8873
Paul Pearce <pearce@cs.berkeley.edu>
2017-10-18 15:43:22 -0700

    Allow `dig like` resolution of a single name from the cmd line. (#136)
    * Allow `dig like` resolution of a single name from the cmd line.
    
    Usage:
    
    $ zdns A example.com | jq "."
    {
      "data": {
        "flags": {
          "error_code": 0,
          "response": true,
          "opcode": 0,
          "authoritative": false,
          "truncated": false,
          "recursion_desired": true,
          "recursion_available": true,
          "authenticated": false,
          "checking_disabled": false
        },
        "protocol": "udp",
        "authorities": [],
        "additionals": [],
        "answers": [
          {
            "answer": "93.184.216.34",
            "name": "example.com",
            "class": "IN",
            "type": "A",
            "ttl": 63020
          }
        ]
      },
      "timestamp": "2017-10-08T18:20:45-07:00",
      "status": "NOERROR",
      "class": "IN",
      "name": "example.com"
    }
    
    * Suggested chagnes from PR comments

--- lookup.go
+++ lookup.go
@@ -157,7 +157,18 @@ func doOutput(out <-chan string, path string, wg *sync.WaitGroup) error {
 }
 
 // read input file and put results into channel
-func doInput(in chan<- interface{}, path string, wg *sync.WaitGroup, zonefileInput bool) error {
+func doInput(in chan<- interface{}, path string, wg *sync.WaitGroup, zonefileInput bool, passedName string) error {
+
+	defer func() {
+		close(in)
+		(*wg).Done()
+	}()
+
+	if len(passedName) > 0 {
+		in <- passedName
+		return nil
+	}
+
 	var f *os.File
 	if path == "" || path == "-" {
 		f = os.Stdin
@@ -182,8 +193,7 @@ func doInput(in chan<- interface{}, path string, wg *sync.WaitGroup, zonefileInp
 			log.Fatal("input unable to read file", err)
 		}
 	}
-	close(in)
-	(*wg).Done()
+
 	return nil
 }
 
@@ -213,7 +223,7 @@ func DoLookups(g *GlobalLookupFactory, c *GlobalConf) error {
 	metaChan := make(chan routineMetadata, c.Threads)
 	var routineWG sync.WaitGroup
 	go doOutput(outChan, c.OutputFilePath, &routineWG)
-	go doInput(inChan, c.InputFilePath, &routineWG, (*g).ZonefileInput())
+	go doInput(inChan, c.InputFilePath, &routineWG, (*g).ZonefileInput(), c.PassedName)
 	routineWG.Add(2)
 	// create pool of worker goroutines
 	var lookupWG sync.WaitGroup
