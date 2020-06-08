123e9299b49ae7c18c4e94f95492fa941d846e37
Zakir Durumeric <zakird@gmail.com>
2020-06-07 21:51:29 -0500

    more test breakage

--- modules/miekg/miekg_test.go
+++ modules/miekg/miekg_test.go
@@ -149,13 +149,13 @@ func verifyResult(t *testing.T, answer interface{}, original dns.RR, expectedAns
 	if ans.Name != original.Header().Name {
 		t.Errorf("Unxpected name. Expected %v, got %v", original.Header().Name, ans.Name)
 	}
-	if ans.rrType != original.Header().Rrtype {
+	if ans.RrType != original.Header().Rrtype {
 		t.Errorf("Unxpected RR type. Expected %v, got %v", original.Header().Rrtype, ans.rrType)
 	}
 	if ans.Type != dns.TypeToString[original.Header().Rrtype] {
 		t.Errorf("Unxpected RR type (string). Expected %v, got %v", dns.TypeToString[original.Header().Rrtype], ans.Type)
 	}
-	if ans.rrClass != original.Header().Class {
+	if ans.RrClass != original.Header().Class {
 		t.Errorf("Unxpected RR class. Expected %v, got %v", original.Header().Class, ans.rrClass)
 	}
 	if ans.Class != dns.ClassToString[original.Header().Class] {
