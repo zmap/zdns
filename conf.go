1ce9a39a217ed4a6321b52e473cf24f191b2d780
Zakir Durumeric <zakird@gmail.com>
2021-01-06 09:23:11 -0800

    `meta_data` -> `metadata`

--- conf.go
+++ conf.go
@@ -79,7 +79,7 @@ type Result struct {
 	Nameserver  string        `json:"nameserver,omitempty" groups:"normal,long,trace"`
 	Class       string        `json:"class,omitempty" groups:"long,trace"`
 	AlexaRank   int           `json:"alexa_rank,omitempty" groups:"short,normal,long,trace"`
-	Metadata    string        `json:"meta_data,omitempty" groups:"short,normal,long,trace"`
+	Metadata    string        `json:"metadata,omitempty" groups:"short,normal,long,trace"`
 	Status      string        `json:"status,omitempty" groups:"short,normal,long,trace"`
 	Error       string        `json:"error,omitempty" groups:"short,normal,long,trace"`
 	Timestamp   string        `json:"timestamp,omitempty" groups:"short,normal,long,trace"`
