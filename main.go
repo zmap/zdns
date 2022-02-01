44a00048eaa9b1a2e03088e77c1027a6ef2b9264
Spencer Drakontaidis <spencerdrak@gmail.com>
2022-02-01 11:44:18 -0800

    import packages to properly populate lookups

--- main.go
+++ main.go
@@ -4,7 +4,17 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/zmap/zdns/cmd"
+import (
+	"github.com/zmap/zdns/cmd"
+	_ "github.com/zmap/zdns/pkg/alookup"
+	_ "github.com/zmap/zdns/pkg/axfr"
+	_ "github.com/zmap/zdns/pkg/bindversion"
+	_ "github.com/zmap/zdns/pkg/dmarc"
+	_ "github.com/zmap/zdns/pkg/miekg"
+	_ "github.com/zmap/zdns/pkg/mxlookup"
+	_ "github.com/zmap/zdns/pkg/nslookup"
+	_ "github.com/zmap/zdns/pkg/spf"
+)
 
 func main() {
 	cmd.Execute()
--- pkg/zdns/zdns.go
+++ pkg/zdns/zdns.go
@@ -11,7 +11,6 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
-
 	"github.com/zmap/dns"
 	"github.com/zmap/zdns/internal/util"
 	"github.com/zmap/zdns/iohandlers"
@@ -130,8 +129,7 @@ func Run(gc GlobalConf, flags *pflag.FlagSet,
 		log.Info("using local address: ", localaddr_string)
 		gc.LocalAddrSpecified = true
 	}
-	log.Info(*localif_string)
-	log.Info(gc.LocalAddrSpecified)
+
 	if *localif_string != "" {
 		if gc.LocalAddrSpecified {
 			log.Fatal("Both --local-addr and --local-interface specified.")
