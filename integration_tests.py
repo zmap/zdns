a987a7a47b3cf7f18ba880acae24cb4f57922ca8
Filip <filip.savin@cujo.com>
2022-12-19 13:56:02 +0200

    Fix broken test function
    assertEqualAnswers() was asserting None == None
    since _lowercase() function do not return anything

--- integration_tests.py
+++ integration_tests.py
@@ -325,7 +325,49 @@ class Tests(unittest.TestCase):
         "class": "IN",
         "name": "large-text.zdns-testing.com",
         "answer": "We present the scanner architecture, experimentally characterize its performance and accuracy, "
-      }
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      },
+      {
+        "type": "TXT",
+        "class": "IN",
+        "name": "large-text.zdns-testing.com",
+        "answer": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
+      },
     ]
 
     WWW_CNAME_ANSWERS = [
@@ -417,7 +459,9 @@ class Tests(unittest.TestCase):
                         obj[k] = v.lower()
                     else:
                         _lowercase(v)
-        self.assertEqual(_lowercase(a), _lowercase(b), helptext)
+        _lowercase(a)
+        _lowercase(b)
+        self.assertEqual(a, b, helptext)
 
     def assertEqualNXDOMAIN(self, res, correct):
         self.assertEqual(res["name"], correct["name"])
