dec7f036eba678533d62e979568f81eb8977ecd1
phillip-stephens <phillip@cs.stanford.edu>
2024-05-28 13:59:31 -0700

    cleaning up unneeded files and added some logs

--- largescaleintegration/validators.py
+++ largescaleintegration/validators.py
@@ -13,31 +13,6 @@ import json
 
 ZDNS_EXECUTABLE = "./zdns"
 
-
-# async def can_visit_with_puppeteer(domain: str, ip_address: str) -> bool:
-#     browser = await launch(headless=True, ignoreHTTPSErrors=True)
-#     page = await browser.newPage()
-#
-#     try:
-#         await page.setExtraHTTPHeaders({'Host': domain,
-#                                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15'})
-#         response = await page.goto(f"https://{ip_address}", {'waitUntil': 'networkidle0'})
-#
-#         # Check if the navigation was successful (status code 200)
-#         if response.ok:
-#             # Take a screenshot of the page
-#             await page.screenshot({'path': '~/zdns/zdns-working/largescaleintegration/screenshot.png'})
-#             return True
-#         else:
-#             return False
-#
-#     except Exception as e:
-#         print(f"An error occurred with domain {domain} at IP {ip_address}: {e}")
-#         return False
-#     finally:
-#         await browser.close()
-
-
 def can_request_successfully(domain: str, ip: str) -> bool:
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"
     headers = {"Host": domain, "User-Agent": user_agent}
@@ -55,10 +30,6 @@ def can_request_successfully(domain: str, ip: str) -> bool:
 def can_automatically_visit(domain: str, ip: str):
     if can_request_successfully(domain, ip):
         return (domain, ip, 0)
-    # if domain == "arca.live":
-    #     ip = "1.1.1.1"
-    # if await can_visit_with_puppeteer(domain, ip):
-    #     return 1
     return (domain, ip, -1)
 
 
@@ -114,7 +85,6 @@ def get_requestable_domains(domains: List[str]) -> List[str]:
             domain_ip_pairs.append((domain, ip))
 
     successes_requests = []
-    successes_puppeteer = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = [executor.submit(can_automatically_visit, domain, ip) for domain, ip in domain_ip_pairs]
 
@@ -131,7 +101,7 @@ def get_requestable_domains(domains: List[str]) -> List[str]:
     # print(f"Successfully visited {len(successes_puppeteer)} out of {len(domains)} domains with puppeteer")
     print("Requests successes:", successes_requests)
     # print("Puppeteer successes:", successes_puppeteer)
-    print("Failures:", set(domains) - set(successes_requests) - set(successes_puppeteer))
+    print("Failures:", set(domains) - set(successes_requests))
     return successes_requests
 
 
@@ -169,7 +139,8 @@ def get_zdns_results_a_lookup(domains: List[str]) -> List[tuple[str, str]]:
 
 
 class TestZDNS(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         with open("top-1k.csv", "r") as f:
             domains = f.read().splitlines()
 
@@ -177,16 +148,18 @@ class TestZDNS(unittest.TestCase):
         # These are those domains which we can successfully request using the requests library directed at an IP address
         # This excludes domains that use some form of DDoS mitigation, such as Cloudflare, which have more sophisticated
         # bot detection
-        self.known_reachable_domains = get_requestable_domains(domains)
+        cls.known_reachable_domains = get_requestable_domains(domains)
 
     def test_zdns_a(self):
         zdns = get_zdns_results_a(self.known_reachable_domains)
+        print(f"ZDNS resolved {len(zdns)} domains to IP addresses from request-able domains")
         for domain, ip in zdns:
             self.assertTrue(can_request_successfully(domain, ip),
                             "ZDNS resolved a domain to an IP address that will not respond to requests for the given domain")
 
     def test_zdns_a_lookup(self):
         zdns = get_zdns_results_a_lookup(self.known_reachable_domains)
+        print(f"ZDNS resolved {len(zdns)} domains to IP addresses from request-able domains")
         for domain, ip in zdns:
             self.assertTrue(can_request_successfully(domain, ip), "ZDNS resolved a domain to an IP address that will not respond to requests for the given domain")
 
