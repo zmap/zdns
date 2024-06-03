f85c80b206a04d6c1d4bba5d98f8a4184a6ed7a6
phillip-stephens <phillip@cs.stanford.edu>
2024-06-03 13:19:42 -0700

    working test

--- testing/large_scan_integration_test.py
+++ testing/large_scan_integration_test.py
@@ -22,7 +22,6 @@ with open(EXPECTED_TEST_FILE, 'r') as f:
         expected_test_data_lookup_domain_to_IPs[data['domain']] = ip_set
 
 
-# run zdns by inputting the domain names
 
 def run_zdns(flags, executable=ZDNS_EXECUTABLE):
     input_domains = [data['domain'] for data in expected_test_data]
@@ -37,11 +36,10 @@ def run_zdns(flags, executable=ZDNS_EXECUTABLE):
 
     # Send the input data to the subprocess and get the output
     stdout, stderr = process.communicate(input=input_data.encode())
-
     return stdout.decode(), stderr.decode()
 
 
-def get_IPs_from_actual_line(actual_line):
+def get_IPs_from_actual_line_a_module(actual_line):
     if 'data' not in actual_line:
         return []
     if 'answers' not in actual_line['data']:
@@ -50,11 +48,21 @@ def get_IPs_from_actual_line(actual_line):
     for answer in actual_line['data']['answers']:
         if 'answer' in answer:
             IPs.append(answer['answer'])
-    return IPs
+    return set(IPs)
+
+def get_IPs_from_actual_line_alookup_module(actual_line):
+    if 'data' not in actual_line:
+        return []
+    if 'ipv4_addresses' not in actual_line['data']:
+        return []
+    IPs = []
+    for IP in actual_line['data']['ipv4_addresses']:
+        IPs.append(IP)
+    return set(IPs)
 
 
-def verify_output(stdout):
-    # parse the output of zdns
+# verify_output takes the stdout from zdns and the module that was run and verifies that the output matches the expected output
+def verify_output(stdout, module):
     # verify that the output matches the expected output
     output_lines = stdout.split("\n")
     for line in output_lines:
@@ -67,15 +75,25 @@ def verify_output(stdout):
         actual_status = actual_line['status']
         if actual_status != "NOERROR":
             assert actual_status == "NOERROR", f"Status is not NOERROR: {actual_status}"
+        if module == "A":
+            actual_IPs = get_IPs_from_actual_line_a_module(actual_line)
+        elif module == "ALOOKUP":
+            actual_IPs = get_IPs_from_actual_line_alookup_module(actual_line)
+        else:
+            assert False, f"Invalid module: {module}"
 
-        actual_ips = set(get_IPs_from_actual_line(actual_line))
-
-        assert set(actual_ips) == set(expected_test_data_lookup_domain_to_IPs[actual_domain]), (
+        assert actual_IPs == set(expected_test_data_lookup_domain_to_IPs[actual_domain]), (
             f"IPs do not match for domain: {actual_domain}\n"
             f"Expected: {expected_test_data_lookup_domain_to_IPs[actual_domain]}\n"
-            f"Actual: {actual_ips}")
+            f"Actual: {actual_IPs}")
 
 
+print("Beginning tests")
+# it seems using the default of 1000 threads gets us rate-limited, downgrading to 100 threads
 std_out, std_err = run_zdns(["A", "--iterative", "--threads", "100"])
-verify_output(std_out)
+verify_output(std_out, "A")
+print("A module passed")
+std_out, std_err = run_zdns(["ALOOKUP", "--iterative", "--threads", "100"])
+verify_output(std_out, "ALOOKUP")
+print("ALOOKUP module passed")
 print("All tests passed")
