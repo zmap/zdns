5af866fbf0f6112aa46024834a8415320bd9ed47
phillip-stephens <phillip@cs.stanford.edu>
2024-06-05 14:51:34 -0700

    removed unused statuses

--- src/zdns/conf.go
+++ src/zdns/conf.go
@@ -38,7 +38,6 @@ const (
 	STATUS_FORMERR   Status = "FORMERR" // Format Error
 	STATUS_SERVFAIL  Status = "SERVFAIL"
 	STATUS_NXDOMAIN  Status = "NXDOMAIN"
-	STATUS_NOTIMP    Status = "NOT_IMPL"
 	STATUS_REFUSED   Status = "REFUSED"
 	STATUS_TRUNCATED Status = "TRUNCATED"
 
@@ -52,7 +51,6 @@ const (
 	STATUS_TIMEOUT       Status = "TIMEOUT"
 	STATUS_ITER_TIMEOUT  Status = "ITERATIVE_TIMEOUT"
 	STATUS_NOAUTH        Status = "NOAUTH"
-	STATUS_NODATA        Status = "NODATA"
 )
 
 var RootServers = [...]string{
