39c79291154ed0888fd9bb474a1aab903b31eef5
phillip-stephens <phillip@cs.stanford.edu>
2024-07-26 11:25:19 -0400

    benchmark looking pretty good

--- benchmark/main.go
+++ benchmark/main.go
@@ -15,46 +15,25 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"github.com/schollz/progressbar/v3"
 	log "github.com/sirupsen/logrus"
 	"io"
 	_ "net/http/pprof"
 	"os"
 	"os/exec"
+	"time"
+
+	"github.com/zmap/zdns/src/zdns"
 )
 
-func initZDNS() (cmd *exec.Cmd, stdin io.WriteCloser, stderr io.ReadCloser, err error) {
-	return cmd, stdin, stderr, nil
-}
+const (
+	linesOfInput = 10 // number of lines to read from input file to feed to ZDNS
+	ZDNSThreads  = 10
+)
 
-func main() {
-	// ZDNS can start a pprof server if the ZDNS_PPROF environment variable is set
-	if err := os.Setenv("ZDNS_PPROF", "true"); err != nil {
-		log.Panicf("failed to set ZDNS_PPROF environment variable: %v", err)
-	}
-
-	cmd := exec.Command("../zdns", "A", "--iterative", "--verbosity=3", "--threads=1")
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		log.Panicf("failed to create stdin pipe: %v", err)
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Panicf("failed to create stderr pipe: %v", err)
-	}
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Panicf("failed to create stdout pipe: %v", err)
-	}
-
-	go func() {
-		// Constantly read from stdout and print
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			fmt.Println("StdOut: " + scanner.Text())
-		}
-	}()
-
+func feedZDNS(inputLines int, stdin io.WriteCloser) {
 	// read in input to stdin
 	go func() {
 		f, err := os.Open("10k_domains.input")
@@ -73,28 +52,149 @@ func main() {
 				log.Panicf("failed to close stdin: %v", err)
 			}
 		}(stdin)
-		// read each line of the file
-		// testing: only read 10 lines
-		lines := 0
 		// Create a new scanner
 		scanner := bufio.NewScanner(f)
 
 		// Read the file line by line
+		linesRead := 0
 		for scanner.Scan() {
-			if lines == 10 {
+			if linesRead >= inputLines {
 				break
 			}
-			lines++
+			linesRead++
 			line := scanner.Text() // Get the current line as a string
-			fmt.Println(line)      // Print the line (or process it as needed)
 			_, err = stdin.Write([]byte(line + "\n"))
 			if err != nil {
 				log.Panicf("failed to write (%v) to stdin: %v", line, err)
 			}
 		}
 	}()
+}
+
+type Stats struct {
+	StartTime             time.Time                // when the benchmark started
+	MinResolveTime        time.Duration            // minimum resolution time for a single domain
+	MaxResolveTime        time.Duration            // maximum resolution time for a single domain
+	AverageResolveTime    time.Duration            // average resolution time for all domains
+	TenLongestResolutions map[string]time.Duration // ten longest resolution times and the domain they belong to
+
+	fastestResolveOfLongest time.Duration // fastest resolution time of the ten longest resolutions, used to know if a new resolution time is in the top ten
+	numberOfResolutions     int           // number of resolutions, used to calculate rolling average
+}
+
+func printStats(s *Stats) {
+	// Define the width for alignment
+	titleWidth := 40
+	timeWidth := 15
+
+	// Print the benchmark duration
+	fmt.Printf("%-*s %*v\n", titleWidth, "Benchmark took:", timeWidth, time.Since(s.StartTime))
+
+	// Print the min, max, and average resolution times
+	fmt.Printf("%-*s %*v\n", titleWidth, "Min resolution time:", timeWidth, s.MinResolveTime)
+	fmt.Printf("%-*s %*v\n", titleWidth, "Max resolution time:", timeWidth, s.MaxResolveTime)
+	fmt.Printf("%-*s %*v\n", titleWidth, "Average resolution time:", timeWidth, s.AverageResolveTime)
+
+	// Print the ten longest resolutions with formatting for alignment
+	fmt.Println("Ten longest resolutions:")
+	for domain, t := range s.TenLongestResolutions {
+		fmt.Printf("\t%-*s %*v\n", titleWidth-8, domain+":", timeWidth, t)
+	}
+}
+
+func updateStats(line string, s *Stats) {
+	var res zdns.Result
+	// parse the line, extracting the domain and resolution time
+	// Unmarshal the JSON string into the struct
+	err := json.Unmarshal([]byte(line), &res)
+	if err != nil {
+		log.Panicf("failed to unmarshal JSON (%s): %v", line, err)
+	}
+	domainName := res.Name
+	resolveTime := time.Duration(res.Duration * float64(time.Second))
+
+	if resolveTime < s.MinResolveTime || s.MinResolveTime == 0 {
+		s.MinResolveTime = resolveTime
+	}
+	if resolveTime > s.MaxResolveTime {
+		s.MaxResolveTime = resolveTime
+	}
+	// Average calculation
+	if s.numberOfResolutions == 0 {
+		s.AverageResolveTime = resolveTime
+	} else {
+		s.AverageResolveTime = (s.AverageResolveTime*time.Duration(s.numberOfResolutions) + resolveTime) / time.Duration(s.numberOfResolutions+1)
+	}
+	// update the ten longest resolutions
+	if len(s.TenLongestResolutions) < 10 {
+		s.TenLongestResolutions[domainName] = resolveTime
+		if resolveTime > s.fastestResolveOfLongest {
+			s.fastestResolveOfLongest = resolveTime
+		}
+	} else if resolveTime > s.fastestResolveOfLongest {
+		// find the fastest resolution time of the ten longest resolutions to remove
+		minTime := time.Hour
+		domainWithFastest := ""
+		// find the domain with the fastest resolution time
+		for domain, time := range s.TenLongestResolutions {
+			if time < minTime {
+				domainWithFastest = domain
+				minTime = time
+			}
+		}
+		// remove the domain with the fastest resolution time
+		delete(s.TenLongestResolutions, domainWithFastest)
+		// add the new domain to the ten longest resolutions
+		s.TenLongestResolutions[domainName] = resolveTime
+	}
+	s.numberOfResolutions++
+}
+
+func processOutput(stdout io.ReadCloser, s *Stats) {
+	go func() {
+		// update user with progress of benchmark
+		bar := progressbar.Default(linesOfInput)
+		bar.Describe(fmt.Sprintf("Benchmarking ZDNS, Resolving %d domains...", linesOfInput))
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			if err := bar.Add(1); err != nil {
+				log.Panicf("failed to update progress bar: %v", err)
+			}
+			updateStats(scanner.Text(), s)
+		}
+	}()
+}
+
+func main() {
+	s := Stats{
+		StartTime:             time.Now(),
+		TenLongestResolutions: make(map[string]time.Duration, 10),
+		numberOfResolutions:   0,
+	}
+	// ZDNS can start a pprof server if the ZDNS_PPROF environment variable is set
+	if err := os.Setenv("ZDNS_PPROF", "true"); err != nil {
+		log.Panicf("failed to set ZDNS_PPROF environment variable: %v", err)
+	}
+
+	cmd := exec.Command("../zdns", "A", "--iterative", "--verbosity=3", "--threads", fmt.Sprintf("%d", ZDNSThreads))
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Panicf("failed to create stdin pipe: %v", err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Panicf("failed to create stderr pipe: %v", err)
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Panicf("failed to create stdout pipe: %v", err)
+	}
+
+	feedZDNS(linesOfInput, stdin)
+	processOutput(stdout, &s)
 
 	// start the command
+
 	err = cmd.Start()
 	if err != nil {
 		log.Panicf("failed to start command: %v", err)
@@ -110,5 +210,7 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Command finished with error:", err)
 		return
 	}
+
+	printStats(&s)
 
 }
--- go.mod
+++ go.mod
@@ -24,11 +24,14 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.6.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/schollz/progressbar/v3 v3.14.4 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
@@ -38,7 +41,8 @@ require (
 	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
