2de3e780753d5f1b9005e10511571f7c6400f71a
phillip-stephens <phillip@cs.stanford.edu>
2024-11-12 14:30:46 -0500

    pr review, remove rename of file handler

--- src/cli/iohandlers/file_handlers.go
+++ src/cli/iohandlers/file_handlers.go
@@ -60,17 +60,17 @@ func (h *FileInputHandler) FeedChannel(in chan<- string, wg *sync.WaitGroup) err
 	return nil
 }
 
-type St struct {
+type FileOutputHandler struct {
 	filepath string
 }
 
-func NewFileOutputHandler(filepath string) *St {
-	return &St{
+func NewFileOutputHandler(filepath string) *FileOutputHandler {
+	return &FileOutputHandler{
 		filepath: filepath,
 	}
 }
 
-func (h *St) WriteResults(results <-chan string, wg *sync.WaitGroup) error {
+func (h *FileOutputHandler) WriteResults(results <-chan string, wg *sync.WaitGroup) error {
 	defer (*wg).Done()
 
 	var f *os.File
