96a721ea2e6c0b38cb1e9f70abc3fb5ca598252a
phillip-stephens <phillip@cs.stanford.edu>
2024-11-26 18:06:31 -0500

    add domain name to root servers default

---
+++ examples/all_nameservers_lookup/main.go
@@ -0,0 +1,1 @@
+package all_nameservers_lookup
--- src/zdns/conf.go
+++ src/zdns/conf.go
@@ -52,35 +52,35 @@ const (
 )
 
 var RootServersV4 = []NameServer{
-	{IP: net.ParseIP("198.41.0.4"), Port: 53},     // A
-	{IP: net.ParseIP("170.247.170.2"), Port: 53},  // B - Changed several times, this is current as of July '24
-	{IP: net.ParseIP("192.33.4.12"), Port: 53},    // C
-	{IP: net.ParseIP("199.7.91.13"), Port: 53},    // D
-	{IP: net.ParseIP("192.203.230.10"), Port: 53}, // E
-	{IP: net.ParseIP("192.5.5.241"), Port: 53},    // F
-	{IP: net.ParseIP("192.112.36.4"), Port: 53},   // G
-	{IP: net.ParseIP("198.97.190.53"), Port: 53},  // H
-	{IP: net.ParseIP("192.36.148.17"), Port: 53},  // I
-	{IP: net.ParseIP("192.58.128.30"), Port: 53},  // J
-	{IP: net.ParseIP("193.0.14.129"), Port: 53},   // K
-	{IP: net.ParseIP("199.7.83.42"), Port: 53},    // L
-	{IP: net.ParseIP("202.12.27.33"), Port: 53},   // M
+	{IP: net.ParseIP("198.41.0.4"), Port: 53, DomainName: "a.root-servers.net"},     // A
+	{IP: net.ParseIP("170.247.170.2"), Port: 53, DomainName: "b.root-servers.net"},  // B - Changed several times, this is current as of July '24
+	{IP: net.ParseIP("192.33.4.12"), Port: 53, DomainName: "c.root-servers.net"},    // C
+	{IP: net.ParseIP("199.7.91.13"), Port: 53, DomainName: "d.root-servers.net"},    // D
+	{IP: net.ParseIP("192.203.230.10"), Port: 53, DomainName: "e.root-servers.net"}, // E
+	{IP: net.ParseIP("192.5.5.241"), Port: 53, DomainName: "f.root-servers.net"},    // F
+	{IP: net.ParseIP("192.112.36.4"), Port: 53, DomainName: "g.root-servers.net"},   // G
+	{IP: net.ParseIP("198.97.190.53"), Port: 53, DomainName: "h.root-servers.net"},  // H
+	{IP: net.ParseIP("192.36.148.17"), Port: 53, DomainName: "i.root-servers.net"},  // I
+	{IP: net.ParseIP("192.58.128.30"), Port: 53, DomainName: "j.root-servers.net"},  // J
+	{IP: net.ParseIP("193.0.14.129"), Port: 53, DomainName: "k.root-servers.net"},   // K
+	{IP: net.ParseIP("199.7.83.42"), Port: 53, DomainName: "l.root-servers.net"},    // L
+	{IP: net.ParseIP("202.12.27.33"), Port: 53, DomainName: "m.root-servers.net"},   // M
 }
 
 var RootServersV6 = []NameServer{
-	{IP: net.ParseIP("2001:503:ba3e::2:30"), Port: 53}, // A
-	{IP: net.ParseIP("2801:1b8:10::b"), Port: 53},      // B
-	{IP: net.ParseIP("2001:500:2::c"), Port: 53},       // C
-	{IP: net.ParseIP("2001:500:2d::d"), Port: 53},      // D
-	{IP: net.ParseIP("2001:500:a8::e"), Port: 53},      // E
-	{IP: net.ParseIP("2001:500:2f::f"), Port: 53},      // F
-	{IP: net.ParseIP("2001:500:12::d0d"), Port: 53},    // G
-	{IP: net.ParseIP("2001:500:1::53"), Port: 53},      // H
-	{IP: net.ParseIP("2001:7fe::53"), Port: 53},        // I
-	{IP: net.ParseIP("2001:503:c27::2:30"), Port: 53},  // J
-	{IP: net.ParseIP("2001:7fd::1"), Port: 53},         // K
-	{IP: net.ParseIP("2001:500:9f::42"), Port: 53},     // L
-	{IP: net.ParseIP("2001:dc3::35"), Port: 53},        // M
+	{IP: net.ParseIP("2001:503:ba3e::2:30"), Port: 53, DomainName: "a.root-servers.net"}, // A
+	{IP: net.ParseIP("2801:1b8:10::b"), Port: 53, DomainName: "b.root-servers.net"},      // B
+	{IP: net.ParseIP("2001:500:2::c"), Port: 53, DomainName: "c.root-servers.net"},       // C
+	{IP: net.ParseIP("2001:500:2d::d"), Port: 53, DomainName: "d.root-servers.net"},      // D
+	{IP: net.ParseIP("2001:500:a8::e"), Port: 53, DomainName: "e.root-servers.net"},      // E
+	{IP: net.ParseIP("2001:500:2f::f"), Port: 53, DomainName: "f.root-servers.net"},      // F
+	{IP: net.ParseIP("2001:500:12::d0d"), Port: 53, DomainName: "g.root-servers.net"},    // G
+	{IP: net.ParseIP("2001:500:1::53"), Port: 53, DomainName: "h.root-servers.net"},      // H
+	{IP: net.ParseIP("2001:7fe::53"), Port: 53, DomainName: "i.root-servers.net"},        // I
+	{IP: net.ParseIP("2001:503:c27::2:30"), Port: 53, DomainName: "j.root-servers.net"},  // J
+	{IP: net.ParseIP("2001:7fd::1"), Port: 53, DomainName: "k.root-servers.net"},         // K
+	{IP: net.ParseIP("2001:500:9f::42"), Port: 53, DomainName: "l.root-servers.net"},     // L
+	{IP: net.ParseIP("2001:dc3::35"), Port: 53, DomainName: "m.root-servers.net"},        // M
 }
 
 var DefaultExternalResolversV4 = []NameServer{
