19baf57400f97622e843c6df54f0f422f4b3ac96
phillip-stephens <phillip@cs.stanford.edu>
2026-05-28 14:18:34 +1200

    make rate limit msg more clear if ctx has already expired

--- src/zdns/rate_limit.go
+++ src/zdns/rate_limit.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/jellydator/ttlcache/v3"
+	"github.com/zmap/zdns/v2/src/internal/util"
 	"golang.org/x/time/rate"
 )
 
@@ -101,6 +102,9 @@ func (l *PerIPPerNameNSRateLimiter) getReservations(ns NameServer) (perIPReserva
 }
 
 func (l *PerIPPerNameNSRateLimiter) wait(ctx context.Context, ns NameServer) error {
+	if util.HasCtxExpired(ctx) {
+		return errors.New("context has already expired before waiting on rate limiter")
+	}
 
 	ipReservation, nameReservation, err := l.getReservations(ns)
 	if err != nil {
